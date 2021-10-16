59e3cf5d5508427abd5978753eb517abea56d297
Pegasoos <circumambulation24@yahoo.com>
2021-10-16 18:45:54 -0400

    query to add department coded

--- db/index.js
+++ db/index.js
@@ -1,1 +1,15 @@
 const connection = require("./connection");
+
+class Query{
+addDepartment(answers){
+let query = connection.query(
+    "INSERT INTO employees_db.employee_department ?",
+     answers,
+     function(err, res){
+         if(err) throw err;
+         console.log(`${res.affectedRows} department added!`)
+     })};
+
+    };
+
+module.exports = new Query();
