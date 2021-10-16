4676e2b7d2d027669ee705ec749362f50e153f98
Pegasoos <circumambulation24@yahoo.com>
2021-10-16 18:54:14 -0400

    add employee and role coded

--- db/index.js
+++ db/index.js
@@ -2,14 +2,37 @@ const connection = require("./connection");
 
 class Query{
 addDepartment(answers){
-let query = connection.query(
+    let query = connection.query(
     "INSERT INTO employees_db.employee_department ?",
      answers,
      function(err, res){
          if(err) throw err;
          console.log(`${res.affectedRows} department added!`)
-     })};
-
+     })
+    console.log(query.sql)
     };
+
+addRole(answers){
+    let query = connection.query(
+        "INSERT INTO employees_db.employee_role ?",
+        answers,
+        function(err, res){
+            if(err) throw err;
+            console.log(`${res.affectedRows} role added!`)
+        })
+        console.log(query.sql);
+};
+addEmployees(answers){
+let query = connection.query(
+        "INSERT INTO employees_db.employee ?",
+        answers,
+        function(err, res){
+            if(err) throw err;
+            console.log(`${res.affectedRows} role added!`)
+        })
+        console.log(query.sql);
+}
+};
+
 
 module.exports = new Query();
