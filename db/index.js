e65854dbee7977f0aaf35e369a62543f8a76c301
Pegasoos <circumambulation24@yahoo.com>
2021-10-16 20:17:55 -0400

    added update functions

--- db/index.js
+++ db/index.js
@@ -31,7 +31,43 @@ let query = connection.query(
             console.log(`${res.affectedRows} role added!`)
         })
         console.log(query.sql);
-}
+};
+viewDepartment(){
+    let query = connection.query(
+        "SELECT * FROM employees_db.employee_department",
+        function(err, res){
+            if(err) throw err;
+            console.log(res.affectedRows)
+        })
+        console.log(query.sql);
+};
+viewRole(){
+    let query = connection.query(
+        "SELECT * FROM employees_db.employee_role",
+        function(err, res){
+            if(err) throw err;
+            console.log(res.affectedRows)
+        })
+        console.log(query.sql);
+};
+viewEmployee(){
+    let query = connection.query(
+        "SELECT * FROM employees_db.employee",
+        function(err, res){
+            if(err) throw err;
+            console.log(res.affectedRows)
+        })
+        console.log(query.sql);
+};
+updateRole(answers){
+    let query = connection.query(//placeholder variables to model the function
+        `UPDATE employees_db.employee_role SET role_id = ${answers.second}, title = ${answers.third}, salary = ${answers.fourth} WHERE employees_db.employee.id = ${answers.first}`,
+        function(err, res){
+            if(err) throw err;
+            console.log(res.affectedRows)
+        })
+        console.log(query.sql);
+};
 };
 
 
